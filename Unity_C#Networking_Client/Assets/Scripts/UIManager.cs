d43c5eb29e97887e4a55b35ed1d029286faff258
BNO0 <tkdfhrtnalsgh@hanmail.net>
2021-03-29 15:06:37 +0900

    test

---
+++ Unity_C#Networking_Client/Assets/Scripts/UIManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    public GameObject startMenu;
+    public InputField usernameField;
+
+    /// <summary>이미 존재하는지 체크</summary>
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Instance already exists, destoying object!");
+            Destroy(this);
+        }
+    }
+
+    /// <summary>연결을 시작하면 UI숨기고 client를 server에 연결</summary>
+    public void ConnectToServer()
+    {
+        startMenu.SetActive(false);
+        usernameField.interactable = false;
+        Client.instance.ConnectToServer();
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
